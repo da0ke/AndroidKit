65f72478a42f04dab6875a06ef9249ebe1f41c00
da0ke <592052922@qq.com>
2018-09-30 16:36:35 +0800

    init

---
+++ app/src/main/java/cn/da0ke/example/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.da0ke.example;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
