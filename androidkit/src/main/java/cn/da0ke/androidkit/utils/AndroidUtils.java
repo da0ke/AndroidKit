936bb5e3d3c3f1ad5fce80fe98707befc2028cc0
da0ke <592052922@qq.com>
2018-10-06 14:06:03 +0800

    新增TakePhotoUtils

---
+++ androidkit/src/main/java/cn/da0ke/androidkit/utils/AndroidUtils.java
@@ -0,0 +1,328 @@
+package cn.da0ke.androidkit.utils;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Point;
+import android.net.Uri;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+import cn.da0ke.androidkit.R;
+
+/**
+ * Created by da0ke on 2018/10/6
+ */
+public class AndroidUtils {
+
+    @SuppressLint("StaticFieldLeak")
+    private static Application sApplication;
+
+    /**
+     * 初始化工具类
+     *
+     * @param app 应用
+     */
+    public static void init(final Application app) {
+        AndroidUtils.sApplication = app;
+    }
+
+    /**
+     * 获取 Application
+     *
+     * @return Application
+     */
+    public static Application getApp() {
+        if (sApplication != null) return sApplication;
+        throw new NullPointerException("你还没有进行初始化，请调用JAndroidTool.init");
+    }
+
+    /**
+     * 启动浏览器打开网页
+     *
+     * @param url 网址
+     *
+     */
+    public static void openChrome(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+
+        //解决部分手机会出错闪退，原因如下：
+        //Calling startActivity() from outside of an Activity context requires the FLAG_ACTIVITY_NEW_TASK flag
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        getApp().startActivity(intent);
+    }
+
+    /**
+     * 跳至拨号界面
+     *
+     * @param phone 电话号码
+     */
+    public static void dial(String phone) {
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phone));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        getApp().startActivity(intent);
+    }
+
+    /**
+     * 显示吐司
+     */
+    public static void showMsg(String msg) {
+        LayoutInflater inflate = (LayoutInflater) getApp().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View toastView = inflate.inflate(R.layout.cn_da0ke_jandroidtool_toast,null);
+        TextView tv = toastView.findViewById(R.id.TextViewInfo);
+        tv.setText(msg);
+        Toast toast = new Toast(getApp());
+        toast.setView(toastView);
+        toast.show();
+    }
+
+    /**
+     * dp 转 px
+     *
+     * @param dpValue dp 值
+     * @return px 值
+     */
+    public static int dp2px(float dpValue) {
+        final float scale = getApp().getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * px 转 dp
+     *
+     * @param pxValue px 值
+     * @return dp 值
+     */
+    public static int px2dp(float pxValue) {
+        final float scale = getApp().getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * 获取屏幕的宽度（单位：px）
+     *
+     * @return 屏幕宽
+     */
+    public static int getScreenWidth() {
+        WindowManager wm = (WindowManager) getApp().getSystemService(Context.WINDOW_SERVICE);
+        if (wm == null) {
+            return getApp().getResources().getDisplayMetrics().widthPixels;
+        }
+        Point point = new Point();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            wm.getDefaultDisplay().getRealSize(point);
+        } else {
+            wm.getDefaultDisplay().getSize(point);
+        }
+        return point.x;
+    }
+
+    /**
+     * 获取屏幕的高度（单位：px）
+     *
+     * @return 屏幕高
+     */
+    public static int getScreenHeight() {
+        WindowManager wm = (WindowManager) getApp().getSystemService(Context.WINDOW_SERVICE);
+        if (wm == null) {
+            return getApp().getResources().getDisplayMetrics().heightPixels;
+        }
+        Point point = new Point();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            wm.getDefaultDisplay().getRealSize(point);
+        } else {
+            wm.getDefaultDisplay().getSize(point);
+        }
+        return point.y;
+    }
+
+    /**
+     * 动态获取资源ID
+     *
+     * @param name The name of the desired resource
+     * @param defType 资源类型，比如"id","drawable"等，对应R.id.xx,R.drawable.xx等
+     * @return int The associated resource identifier.  Returns 0 if no such
+     *         resource was found.  (0 is not a valid resource ID.)
+     */
+    public static int getResourceId(String name, String defType) {
+        return getApp().getResources().getIdentifier(name,defType,getApp().getPackageName());
+    }
+
+    /**
+     * Return the version name of device's system.
+     *
+     * @return the version name of device's system
+     */
+    public static String getSDKVersionName() {
+        return android.os.Build.VERSION.RELEASE;
+    }
+
+    /**
+     * Return version code of device's system.
+     *
+     * @return version code of device's system
+     */
+    public static int getSDKVersionCode() {
+        return android.os.Build.VERSION.SDK_INT;
+    }
+
+    /**
+     * Return the android id of device.
+     *
+     * @return the android id of device
+     */
+    @SuppressLint("HardwareIds")
+    public static String getAndroidID() {
+        String id = Settings.Secure.getString(
+                getApp().getContentResolver(),
+                Settings.Secure.ANDROID_ID
+        );
+        return id == null ? "" : id;
+    }
+
+    /**
+     * Return the MAC address.
+     * <p>Must hold
+     * {@code <uses-permission android:name="android.permission.ACCESS_WIFI_STATE" />},
+     * {@code <uses-permission android:name="android.permission.INTERNET" />}</p>
+     *
+     * @return the MAC address
+     */
+    public static String getMacAddress() {
+        String macAddress = getMacAddressByWifiInfo();
+        if (!"02:00:00:00:00:00".equals(macAddress)) {
+            return macAddress;
+        }
+        macAddress = getMacAddressByNetworkInterface();
+        if (!"02:00:00:00:00:00".equals(macAddress)) {
+            return macAddress;
+        }
+        macAddress = getMacAddressByInetAddress();
+        if (!"02:00:00:00:00:00".equals(macAddress)) {
+            return macAddress;
+        }
+
+        return "";
+    }
+
+    @SuppressLint({"HardwareIds", "MissingPermission"})
+    private static String getMacAddressByWifiInfo() {
+        try {
+            Context context = getApp().getApplicationContext();
+            WifiManager wifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+            if (wifi != null) {
+                WifiInfo info = wifi.getConnectionInfo();
+                if (info != null) return info.getMacAddress();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "02:00:00:00:00:00";
+    }
+
+    private static String getMacAddressByNetworkInterface() {
+        try {
+            Enumeration<NetworkInterface> nis = NetworkInterface.getNetworkInterfaces();
+            while (nis.hasMoreElements()) {
+                NetworkInterface ni = nis.nextElement();
+                if (ni == null || !ni.getName().equalsIgnoreCase("wlan0")) continue;
+                byte[] macBytes = ni.getHardwareAddress();
+                if (macBytes != null && macBytes.length > 0) {
+                    StringBuilder sb = new StringBuilder();
+                    for (byte b : macBytes) {
+                        sb.append(String.format("%02x:", b));
+                    }
+                    return sb.substring(0, sb.length() - 1);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "02:00:00:00:00:00";
+    }
+
+    private static String getMacAddressByInetAddress() {
+        try {
+            InetAddress inetAddress = getInetAddress();
+            if (inetAddress != null) {
+                NetworkInterface ni = NetworkInterface.getByInetAddress(inetAddress);
+                if (ni != null) {
+                    byte[] macBytes = ni.getHardwareAddress();
+                    if (macBytes != null && macBytes.length > 0) {
+                        StringBuilder sb = new StringBuilder();
+                        for (byte b : macBytes) {
+                            sb.append(String.format("%02x:", b));
+                        }
+                        return sb.substring(0, sb.length() - 1);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "02:00:00:00:00:00";
+    }
+
+    private static InetAddress getInetAddress() {
+        try {
+            Enumeration<NetworkInterface> nis = NetworkInterface.getNetworkInterfaces();
+            while (nis.hasMoreElements()) {
+                NetworkInterface ni = nis.nextElement();
+                // To prevent phone of xiaomi return "10.0.2.15"
+                if (!ni.isUp()) continue;
+                Enumeration<InetAddress> addresses = ni.getInetAddresses();
+                while (addresses.hasMoreElements()) {
+                    InetAddress inetAddress = addresses.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        String hostAddress = inetAddress.getHostAddress();
+                        if (hostAddress.indexOf(':') < 0) return inetAddress;
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Return the manufacturer of the product/hardware.
+     * <p>e.g. Xiaomi</p>
+     *
+     * @return the manufacturer of the product/hardware
+     */
+    public static String getManufacturer() {
+        return Build.MANUFACTURER;
+    }
+
+    /**
+     * Return the model of device.
+     * <p>e.g. MI2SC</p>
+     *
+     * @return the model of device
+     */
+    public static String getModel() {
+        String model = Build.MODEL;
+        if (model != null) {
+            model = model.trim().replaceAll("\\s*", "");
+        } else {
+            model = "";
+        }
+        return model;
+    }
+
+}
