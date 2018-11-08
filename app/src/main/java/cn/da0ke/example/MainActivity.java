50ad241b25c31d9c0689feb699f0d5589ca23c9a
da0ke <592052922@qq.com>
2018-11-08 15:56:04 +0800

    ud

--- app/src/main/java/cn/da0ke/example/MainActivity.java
+++ app/src/main/java/cn/da0ke/example/MainActivity.java
@@ -1,10 +1,8 @@
 package cn.da0ke.example;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
-import cn.da0ke.androidkit.map.MapActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,12 +20,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void clickMap() {
-        Intent intent = new Intent(this, MapActivity.class);
-        intent.putExtra("com","苏州某某网络信息技术有限公司");
-        intent.putExtra("address","盛泽镇舜湖西路");
-        intent.putExtra("lat",30.910196);
-        intent.putExtra("lng",120.632369);
-        startActivity(intent);
+
     }
 
 
